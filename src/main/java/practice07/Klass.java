848d0bffbb608028d5c6170591491dd68550d650
jialiu <jialiu@thoughtworks.com>
2019-07-02 17:09:23 +0800

    [liujia] practice7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,16 @@
 package practice07;
 
 public class Klass {
+    long number;
+    public Klass(long number) {
+        this.number = number;
+    }
+
+    public getNumber(){
+        return this.number;
+    }
+
+    public getDisplayName(){
+        return "Class " + this.number;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,18 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    Klass klass;
+
+    public Student(string name, long age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public long getKlass(){
+        return this.klass;
+    }
+
+    public string introduce(){
+        return super.introduce() + "I am a Student. I am at " + klass.getDisplayName(();
+    }
 }
