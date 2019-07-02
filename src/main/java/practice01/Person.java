354f2c8c3eb0112b8133b6683f6875a97983fa20
jialiu <jialiu@thoughtworks.com>
2019-07-02 16:28:22 +0800

    [liujia] practice1

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,18 @@
 package practice01;
 
 public class Person {
+    string name;
+    string age;
+    public Person(string name, long age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public string getName(){return name;}
+
+    public long getAge(){return age;}
+
+    public string introduce() {
+        return "My name is" + this.name + ". I am" + this.age + " years old.";
+    }
 }
