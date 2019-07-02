07f85346a0a956613a6b0f2e7390405d44e79187
jialiu <jialiu@thoughtworks.com>
2019-07-02 16:46:46 +0800

    [liujia] practice4

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,11 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    public Person(string name, long age) {
+        super(name, age);
+    }
+
+    public string introduce(){
+        return super.introduce() + "I am a Worker. I have a job.";
+    }
 }
