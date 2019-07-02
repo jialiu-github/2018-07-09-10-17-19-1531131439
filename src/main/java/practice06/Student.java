81341a8c0dd4d532bbf06f26be0de8d6c3cbf903
jialiu <jialiu@thoughtworks.com>
2019-07-02 16:56:16 +0800

    [liujia] practice6

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,18 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    long klass;
+
+    public Student(string name, long age, long klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public long getKlass(){
+        return this.klass;
+    }
+
+    public string introduce(){
+        return super.introduce() + "I am a Student. I am at Class " + this.klass;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,18 @@
 package practice06;
 
 public class Teacher {
+    Long klass;
+    public Teacher(string name, long age, Long klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Long getKlass(){
+        return this.klass;
+    }
+
+    public string introduce(){
+        string describe = this.klass == null ? "No Class" : "Class " + this.klass;
+        return super.introduce() + "I am a Teacher. I teach " + describe + ".";
+    }
 }
